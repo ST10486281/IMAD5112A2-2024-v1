0ceec30bfdb4c85efb0613bb8ccdf22a4ed1ad79
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-30 17:15:21 +0000

    Test

---
+++ app/src/androidTest/java/com/example/imad5112a2_2024_v1/MainActivityTest.kt
@@ -0,0 +1,11 @@
+package com.example.imad5112a2_2024_v1
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainActivityTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    fun setUp() {
+        
+    }
+}
