2d3f1ba3022491f04581ac9071c2e771e901ffc1
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-04-30 16:28:11 +0000

    Second screen

--- app/src/main/java/com/example/imad5112a2_2024_v1/Report.kt
+++ app/src/main/java/com/example/imad5112a2_2024_v1/Report.kt
@@ -2,10 +2,29 @@ package com.example.imad5112a2_2024_v1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Report : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_report)
+
+
+        val reportTextView = findViewById<TextView>(R.id.reportTextView)
+
+        val bundle: Bundle? = intent.extras
+        val days = bundle?.getStringArray("daysArray")
+        val grams = bundle?.getIntArray("gramsArray")
+
+
+        var total = 0
+
+        if (days != null) {
+            for (i in days.indices) {
+                total = total+grams[i]
+            }
+        }
+
+        reportTextView.text = "Total: $total grams fed this week."
     }
 }
