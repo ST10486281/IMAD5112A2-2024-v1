b5c88d9e33f272383fb57ea46909945e1a392460
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-05-01 13:32:18 +0200

    1

--- app/src/androidTest/java/com/example/imad5112a2_2024_v1/ReportTest.kt
+++ app/src/androidTest/java/com/example/imad5112a2_2024_v1/ReportTest.kt
@@ -13,7 +13,11 @@ class ReportTest {
 
     @Test
     fun testReportDisplaysTotal() {
-        val intent = Intent().apply {
+        val intent = Intent(Intent.ACTION_MAIN).apply {
+            setClassName(
+                "com.example.imad5112a2_2024_v1",
+                "com.example.imad5112a2_2024_v1.Report"
+            )
             putExtra("daysArray", arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"))
             putExtra("gramsArray", intArrayOf(100, 100, 100, 100, 100, 100, 100))
         }
